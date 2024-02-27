8aaf0489d227166ee2007838e6005822cb48151a
samvrny <samvrny@gmail.com>
2024-02-26 23:53:56 -0600

    Got JavaScript for handling fake form submission created

--- javascript/confirmation.js
+++ javascript/confirmation.js
@@ -1,7 +1,13 @@
-function submit() {
-    let form = document.getElementById("confirmationForm");
+let form = document.getElementById("confirmationForm");
 
-    form.preventDefault();
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-    console.log("Hello World")
-}
+    let val1 = form.name;
+    let val2 = form.email;
+
+    console.log(val1, val2);
+    console.log("Hello World");
+
+    window.location.assign("confirmation.html");
+});
