fe2b0d9330a7c077952e9c81af328e1b5e44a803
samvrny <samvrny@gmail.com>
2024-02-27 00:09:53 -0600

    Completed updating the form submission as well as creating a confirmation page

--- javascript/confirmation.js
+++ javascript/confirmation.js
@@ -1,13 +1,10 @@
 let form = document.getElementById("confirmationForm");
 
+/**
+ * When a user submits a form, send them to the confirmation page
+ */
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-
-    let val1 = form.name;
-    let val2 = form.email;
-
-    console.log(val1, val2);
-    console.log("Hello World");
 
     window.location.assign("confirmation.html");
 });
